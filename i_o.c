fac717faeda660f1b5adffa58c4220f4fd176d51
King <louie_101@live.ca>
2019-03-17 15:30:33 -0700

    I\O component done I think

--- i_o.c
+++ i_o.c
@@ -1,13 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include "stonylake/device1.h"
+#include <stdint.h>
+#include "stonylake/devices.h"
+
+uint8_t i_oGetInput(uint8_t fromDeviceNum){
+    return devicesGiveInput(fromDeviceNum);
+};
+void i_oGiveOutput(uint8_t toDeviceNum, uint8_t output){
+    uint8_t rt = devicesGetOutput(toDeviceNum, output);
+    assert(rt == 0b1111111);
+    return;
+};
 
 int main(){
+    uint8_t testDevice = 0b001;
+    uint8_t testGiveOutput = 0b00000111;
+    uint8_t testGetInput;
 
-    int k = 0;
-    k = addOne(k);
-    k = addTwo(k);
-    printf("k = %d\n", k);
+    testGetInput = i_oGetInput(testDevice);
+    i_oGiveOutput(testDevice, testGiveOutput);
+    printf("testGetInput %x", testGetInput);
+
+
     return 0;
 }
