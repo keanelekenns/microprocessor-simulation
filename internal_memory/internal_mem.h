ac47d37da6d8268fa63f9418e0421581db5965d2
nat <nathanc1945@gmail.com>
2019-03-22 11:57:52 -0700

    Scratchpad is an array

--- internal_memory/internal_mem.h
+++ internal_memory/internal_mem.h
@@ -9,18 +9,19 @@
 struct memory
 {
 	
-	uint8_t Accumulator;
-	uint8_t B;
-	uint8_t C;
-	uint8_t D;
-	uint8_t E;
-	uint8_t H;
-	uint8_t L;
+	// scratchpad[0]: accumulator
+	// scratchpad[1]: B
+	// scratchpad[2]: C
+	// scratchpad[3]: D
+	// scratchpad[4]: E
+	// scratchpad[5]: H
+	// scratchpad[6]: L
+	uint8_t scratchpad[7];
 
 	// Stores current program counter; we only use three bits of it.
 	uint8_t Address_Counter;
 
-	// 8 14-bit slocks to store addresses
+	// 8 14-bit slots to store addresses; we only use 14 bits of each of these.
 	uint16_t Address_Stack[8];
 
 	// External memory array
