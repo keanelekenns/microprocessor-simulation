2c8b61fea96a34cd540d1941238c91aef29b78a0
N <nmanning@uvic.ca>
2019-03-26 20:43:58 -0700

    Execute for stages T1 and T2.

--- main.c
+++ main.c
@@ -1,61 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "memory.h"
-#include "state.h"
 #include "decode.h"
 #include "devices.h"
-//#include "ALU.h"
+#include "ALU.h"
 
+#define PCL_MASK 0xFF
+#define L 5
+#define H 6
 
-struct state State;
+void T1_execute(uint8_t control) {
+    uint8_t out_value;
+    switch (control) {
+        case PCL_OUT:
+            out_value = (uint8_t) (mem.program_counter & PCL_MASK);
+            break;
+        case REGL_OUT:
+            out_value = mem.scratch_pad[L];
+    }
 
-int T1_execute(){
-	return 0;
-}
-int T2_execute(){
-	return 0;
-}
-int T3_execute(){
-	return 0;
-}
-int T4_execute(){
-	return 0;
-}
-int T5_execute(){
-	return 0;
+    mem.mem_low = out_value;
 }
 
+void T2_execute(uint8_t control) {
+    uint8_t out_value;
+    switch (control) {
+        case PCH_OUT:
+            out_value = (uint8_t) (mem.program_counter >> 8);
+            break;
+        case REGH_OUT:
+            out_value = mem.scratch_pad[H];
+    }
 
-int main(){
+    mem.mem_high = out_value;
+}
 
-	// Initialize components
-	init_state(State);
-	//should be able to just call mem.<whatyouneed> to access the variables
+void T3_execute() {
+}
 
+void T4_execute() {
+}
 
+void T5_execute() {
+}
+
+DecodeControl decode_control;
+
+int main() {
+    decode_control = init_decode_control(decode_control);
+    uint8_t current_cycle = decode_control.current_cycle;
 	// Main program loop; each iteration of the loop is one processor state.
-	for (;;)
-	{
-		// advance to next TState we have queued
-		update_state(State);
+	T1_execute(decode_control.t1_control[current_cycle]);
 
-		switch (State.current_state){
-			case 1: T1_execute();
-
-			case 2: T2_execute();
-
-			case 3: T3_execute();
-
-			case 4: T4_execute();
-
-			case 5: T5_execute();
-		}
-
-
-		end_state(State);
-	}
-
+    T2_execute(decode_control.t2_control[current_cycle]);
 
 	exit(0);
 }
-
