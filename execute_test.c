810447cb07c06f2c60d4d77c0f8464aec71d0aba
N <nmanning@uvic.ca>
2019-03-29 11:01:21 -0700

    Added tests for T2 and T3 execute.

--- execute_test.c
+++ execute_test.c
@@ -54,7 +54,96 @@ void test_t1_execute() {
     puts("All T1_execute() tests passed.");
 }
 
+void test_t2_execute() {
+    // Test PCH_OUT
+    puts("Testing T2_execute().");
+    control = init_decode_control(control);
+    reset_memory();
+
+    // Set current instruction to index 1153 in memory
+    mem.address_stack[0] = 1153;
+
+    // Execute T2 to get high byte of instruction address
+    T2_execute(control.t2_control[0]);
+    if (mem.mem_high != 4) {
+        puts("PCH_OUT test failed.");
+        exit(0);
+    }
+
+    // Test REGH_OUT
+    // Set control signal and provide test value for register H
+    control.t2_control[0] = REGH_OUT;
+    mem.scratch_pad[H] = 246;
+    // Execute T1 to get byte from register H
+    T2_execute(control.t2_control[0]);
+    if (mem.mem_high != 246) {
+        puts("REGL_OUT test failed.");
+        exit(0);
+    }
+
+    puts("All T2_execute() tests passed.");
+}
+
+void test_t3_execute() {
+    puts("Testing T3_excute().");
+    control = init_decode_control(control);
+    reset_memory();
+
+    // Test instruction fetch/decode
+    control.t3_control[0] = FETCH;
+    // Setup data to load instruction
+    mem.memory[5] = 196;
+    mem.address_stack[0] = 5;
+    mem.mem_low = 5;
+    mem.mem_high = 0;
+    // Run test
+    T3_execute(control.t3_control[0]);
+    if (mem.reg_b != 196 || mem.instruction_reg != 196 || mem.address_stack[0] != 6) {
+        puts("FETCH test failed.");
+        exit(0);
+    }
+
+    // Test REGB_TO_OUT
+    control.t3_control[0] = REGB_TO_OUT;
+    mem.memory[1797] = 73;
+    mem.mem_low = 5;
+    mem.mem_high = 7;
+    mem.reg_b = 7;
+    T3_execute(control.t3_control[0]);
+    if (mem.memory[1797] != 7) {
+        puts("REGB_TO_OUT test failed.");
+        exit(0);
+    }
+
+    // Test DATA_TO_REGB and LOW_ADDR_TO_REGB
+    // Complete the same instruction and only differ by control signal.
+    // This test is for behaviour, so can just test with one signal
+    control.t3_control[0] = DATA_TO_REGB;
+    mem.reg_b = 5;
+    T3_execute(control.t3_control[0]);
+    if (mem.reg_b != 7) {
+        puts("DATA_TO_REGB/LOW_ADDR_TO_REGB test failed.");
+        exit(0);
+    }
+
+    // Test HIGH_ADDR_TO_REGA and HIGH_ADDR_TO_REGA_COND
+    // As with test for DATA_TO_REGB and LOW_ADDR_TO_REGB can just test one
+    control.t3_control[0] = HIGH_ADDR_TO_REGA;
+    mem.reg_a = 5;
+    T3_execute(control.t3_control[0]);
+    if (mem.reg_a != 7) {
+        puts("HIGH_ADDR_TO_REGA/HIGH_ADDR_TO_REGA_COND test failed.");
+        exit(0);
+    }
+
+    puts("All T3_execute() tests passed.");
+}
+
 int main () {
     test_t1_execute();
+    puts("");
+    test_t2_execute();
+    puts("");
+    test_t3_execute();
     return 0;
 }
