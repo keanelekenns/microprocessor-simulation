01e8603eeac72224bf32f8558d7ff16236df3785
King <louie_101@live.ca>
2019-03-30 13:42:24 -0700

    changed code to mirror master as of mar 29

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ CC=gcc
 CFLAGS= -Wall -Werror -g
 
 # Examples for a more complicated Makefile
-DEPS = memory.h alu.h decode.h
-OBJ = main.o memory.o ALU.o decode.c
-SOURCES = main.c memory.c ALU.c decode.c
+DEPS = memory.h alu.h decode.h execute.h
+OBJ = main.o memory.o ALU.o decode.o execute.o
+SOURCES = main.c memory.c ALU.c decode.c execute.c
 
 %.o: $(SOURCES) $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
@@ -15,5 +15,9 @@ SOURCES = main.c memory.c ALU.c decode.c
 main: $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS)
 
+# Compile test cases for execute functions
+execute_test: execute_test.o memory.o ALU.o decode.o execute.o
+
+# Clean up binary and object files
 clean:
-	rm -f *.o main readFile
+	rm -f *.o main execute_test readFile
--- main.c
+++ main.c
@@ -1,90 +1,11 @@
+#include "main.h"
 #include "memory.h"
 #include "decode.h"
 #include "devices.h"
+#include "execute.h"
 #include "ALU.h"
 
-#define PCL_MASK 0xFF
-#define L 5
-#define H 6
-
 DecodeControl control;
-
-void T1_execute(uint8_t t1_control) {
-    uint8_t out_value;
-    switch (t1_control) {
-        case PCL_OUT:
-            out_value = (uint8_t) (mem.address_stack[0] & PCL_MASK);
-            break;
-        case REGL_OUT:
-            out_value = mem.scratch_pad[L];
-    }
-
-    mem.mem_low = out_value;
-}
-
-void T2_execute(uint8_t t2_control) {
-    uint8_t out_value;
-    switch (t2_control) {
-        case PCH_OUT:
-            out_value = (uint8_t) (mem.address_stack[0] >> 8);
-            break;
-        case REGH_OUT:
-            out_value = mem.scratch_pad[H];
-    }
-
-    mem.mem_high = out_value;
-}
-
-void T3_execute(uint8_t t3_control) {
-    uint8_t data_from_memory;
-    uint16_t address = mem.mem_low + (mem.mem_high << 8);
-    switch (t3_control) {
-        case FETCH:
-            data_from_memory = mem.memory[address];
-            mem.reg_b = data_from_memory;
-            mem.instruction_reg = data_from_memory;
-
-            init_decode_control(control);
-            control = decode(control, data_from_memory);
-            mem.address_stack[program_counter] += 1;
-            break;
-        case FETCH_HALT:
-            data_from_memory = mem.memory[address];
-            exit(0); // Halt used as end program
-            break;
-        case REGB_TO_OUT:
-            mem.memory[address] = mem.reg_b;
-            break;
-        case DATA_TO_REGB:
-            mem.reg_b = mem.memory[address];
-            break;
-        case LOW_ADDR_TO_REGB:
-            mem.reg_b = mem.memory[address];
-            break;
-        case HIGH_ADDR_TO_REGA:
-            mem.reg_a = mem.memory[address];
-            break;
-        case HIGH_ADDR_TO_REGA_COND:
-            mem.reg_a = mem.memory[address];
-            break;
-    }
-}
-
-void T4_execute(uint8_t t4_control) {
-    switch (t4_control) {
-        case SSS_TO_REGB:
-            mem.reg_b = mem.scratch_pad[control.source_register];
-            break;
-        case REG_A_TO_PCH:
-            // Clear PCH then copy value from reg a to PCH
-            mem.address_stack[0] = mem.address_stack[0] & PCL_MASK;
-            mem.address_stack[0] += ((uint16_t) mem.reg_a) << 8;
-            break;
-    }
-}
-
-void T5_execute() {
-}
 
 int main() {
     control = init_decode_control(control);
@@ -104,6 +25,14 @@ int main() {
         }
 
         T4_execute(control.t4_control[current_cycle]);
+
+        T5_execute(control.t5_control[current_cycle]);
+
+        control.current_cycle++;
+        // Instruction complete
+        if (control.current_cycle == control.cycle_length) {
+            control = init_decode_control(control);
+        }
     }
 
 	exit(0);
