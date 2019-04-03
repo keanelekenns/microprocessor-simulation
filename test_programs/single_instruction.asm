4a2dfd1ff4ceeca09959ad0106832fe077480638
nat <nathanc1945@gmail.com>
2019-04-03 14:26:53 -0700

    A few more tests

--- test_programs/single_instruction.asm
+++ test_programs/single_instruction.asm
@@ -1,7 +1,8 @@
 00000100 Add fifty seven to accumulator
 00111001
-00100100 isolate middle three bits with and
+00010100 subtract fifty seven to accumulator
 00111001
-00100100 isolate middle three bits with and
-00111001
+01101000 jtz
+00000000
+00000000
 00000001 HLT
